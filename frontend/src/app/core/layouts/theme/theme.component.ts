0c67931fa159f74eef082b9ee5e3d94788c5f550
Alex Rodriguez <alexrodriguezbenitez@gmail.com>
2024-10-29 13:04:27 -0600

    Agregando el estilo del sistio

--- frontend/src/app/core/layouts/theme/theme.component.ts
+++ frontend/src/app/core/layouts/theme/theme.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-theme',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, RouterLink, MatSidenavModule, MatIcon],
   templateUrl: './theme.component.html',
   styleUrl: './theme.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ThemeComponent {}
