10e754ca7a1d4c0213d384339cc3d8931e0f6b30
Alex Rodriguez <alexrodriguezbenitez@gmail.com>
2024-10-29 12:08:54 -0600

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './core/components/app/app.component';
+import { DashboardComponent } from './core/components/dashboard/dashboard.component';
 import { LoginComponent } from './core/components/login/login.component';
+import { ThemeComponent } from './core/layouts/theme/theme.component';
 
 export const routes: Routes = [
   // Aquí definiremos nuestras rutas
   { path: 'login', component: LoginComponent },
-  { path: '', component: AppComponent },
+  {
+    path: '',
+    component: ThemeComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'app', component: AppComponent },
+    ],
+  },
   { path: '**', redirectTo: 'login' },
 ];
