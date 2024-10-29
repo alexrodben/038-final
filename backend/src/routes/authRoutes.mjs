7fa285f1b215f160cc49bf0d82600094ab14fb2f
Alex Rodriguez <alexrodriguezbenitez@gmail.com>
2024-10-29 15:25:43 -0600

    Update authRoutes.mjs

--- backend/src/routes/authRoutes.mjs
+++ backend/src/routes/authRoutes.mjs
@@ -1,20 +1,10 @@
 import { Router } from 'express';
 import { login, profile } from '../controllers/authController.mjs';
-import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from '../controllers/userController.mjs';
-import { authenticateToken } from '../middleware/authMiddleware.mjs';
 
 const router = Router();
 
 // Ruta para login
 router.post('/auth/login', login);
-router.get('/auth/profile', authenticateToken, profile);
-
-// Rutas para usuarios
-router.post('/users', authenticateToken, createUser);
-router.get('/users', authenticateToken, getAllUsers);
-router.get('/users/:id', authenticateToken, getUserById);
-router.put('/users', authenticateToken, updateUser);
-router.delete('/users/:id', authenticateToken, deleteUser);
-
+router.get('/auth/profile', profile);
 
 export default router;
