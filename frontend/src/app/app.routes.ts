2222e6f3ad6f1749fc0b1e8e9c1791d161f65c44
Alex Rodriguez <alexrodriguezbenitez@gmail.com>
2024-10-29 20:33:33 -0600

    Restructurando Login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './core/components/app/app.component';
 import { DashboardComponent } from './core/components/dashboard/dashboard.component';
-import { LoginComponent } from './core/components/login/login.component';
 import { Error404Component } from './core/layouts/error-404/error-404.component';
+import { LoginComponent } from './core/layouts/login/login.component';
 import { ThemeComponent } from './core/layouts/theme/theme.component';
 // Users
 import { UserCreateComponent } from './core/components/user/user-create/user-create.component';
