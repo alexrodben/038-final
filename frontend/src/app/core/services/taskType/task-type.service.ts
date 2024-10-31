d889c926eb44da6280bd6a43d4943844671174bd
Alex Rodriguez <alexrodriguezbenitez@gmail.com>
2024-10-31 12:37:45 -0600

    Update task-type.service.ts

--- frontend/src/app/core/services/taskType/task-type.service.ts
+++ frontend/src/app/core/services/taskType/task-type.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { TaskType } from '../../models/taskType';
+import { TaskTypeModel } from '../../models/taskType';
 import { HttpService } from '../http.services'; // Asegúrate de que la ruta sea correcta
 
 @Injectable({
@@ -10,8 +10,8 @@ import { HttpService } from '../http.services'; // Asegúrate de que la ruta sea
 export class TaskTypeService extends HttpService {
   private baseUrl = `api/task-types`;
 
-  // Método para transformar los datos de JSON a TaskType
-  private transformToTaskType(data: any): TaskType {
+  // Método para transformar los datos de JSON a TaskTypeModel
+  private transformToTaskType(data: any): TaskTypeModel {
     return {
       id: data.id,
       nombre: data.nombre,
@@ -19,31 +19,31 @@ export class TaskTypeService extends HttpService {
   }
 
   // Transformación para un array de tipos de tarea
-  private mapTaskTypes(taskTypeData: any[]): TaskType[] {
+  private mapTaskTypes(taskTypeData: any[]): TaskTypeModel[] {
     return taskTypeData.map(this.transformToTaskType);
   }
 
-  getAllTaskTypes(): Observable<TaskType[]> {
-    return this.get<TaskType[]>(this.baseUrl).pipe(
+  getAllTaskTypes(): Observable<TaskTypeModel[]> {
+    return this.get<TaskTypeModel[]>(this.baseUrl).pipe(
       map((data: any[]) => this.mapTaskTypes(data)) // Aplicamos la transformación aquí
     );
   }
 
-  getTaskTypeById(id: string): Observable<TaskType> {
-    return this.get<TaskType>(`${this.baseUrl}/${id}`).pipe(
+  getTaskTypeById(id: string): Observable<TaskTypeModel> {
+    return this.get<TaskTypeModel>(`${this.baseUrl}/${id}`).pipe(
       map((data: any) => this.transformToTaskType(data)) // Aplicamos la transformación aquí
     );
   }
 
-  createTaskType(taskType: TaskType): Observable<any> {
-    return this.post<TaskType>(this.baseUrl, taskType);
+  createTaskType(taskType: TaskTypeModel): Observable<any> {
+    return this.post<TaskTypeModel>(this.baseUrl, taskType);
   }
 
-  updateTaskType(id: string, taskType: TaskType): Observable<any> {
-    return this.put<TaskType>(`${this.baseUrl}/${id}`, taskType);
+  updateTaskType(id: string, taskType: TaskTypeModel): Observable<any> {
+    return this.put<TaskTypeModel>(`${this.baseUrl}/${id}`, taskType);
   }
 
   deleteTaskType(id: string): Observable<any> {
-    return this.delete<TaskType>(`${this.baseUrl}/${id}`);
+    return this.delete<TaskTypeModel>(`${this.baseUrl}/${id}`);
   }
 }
