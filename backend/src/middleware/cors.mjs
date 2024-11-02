f978adb3eee7bd981106de3584125de6abe40e9b
Alex Rodriguez <alexrodriguezbenitez@gmail.com>
2024-11-02 02:15:13 -0600

    Update cors.mjs

--- backend/src/middleware/cors.mjs
+++ backend/src/middleware/cors.mjs
@@ -1,7 +1,7 @@
 // cors.js
 import cors from 'cors';
 const corsOptions = {
-    origin: 'http://localhost:4200', // Cambia esto según tu necesidad
+    origin: '*', // Cambia esto según tu necesidad
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 };
